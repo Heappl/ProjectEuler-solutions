5151eed641299dd28ff0f5c9b713e4fc65ca89f9
bartek <bartek@bartek-HP-Compaq-8710p.(none)>
2011-06-19 20:25:45 +0200

    bouncy numbers - easy solution

--- Clojure/bouncyNumbers.clj
+++ Clojure/bouncyNumbers.clj
@@ -4,28 +4,16 @@
   (:require [clojure.contrib.math :as math])
   (:require [clojure.set :as set]))
 
-(defn update [hm k v]
-  (let [previous (if (nil? (get hm k)) 0 (get hm k))]
-    (assoc hm k (+ v previous))))
-(defn updateAll [hm [k & ktail] v]
-  (if (nil? k)
-    hm
-    (recur (update hm k v) ktail v)))
+(defn isNotBouncy [x]
+  (loop [nextX (unchecked-divide x 10) dig (rem x 10) op nil]
+		(if (= nextX 0)
+      true
+      (let [aux1 (rem nextX 10) aux2 (unchecked-divide nextX 10)]
+        (if (= aux1 dig) (recur aux2 aux1 op) (if (nil? op)
+                                               (recur aux2 aux1 (if (< aux1 dig) <= >=))
+                                               (if (op aux1 dig) (recur aux2 aux1 op) false)))))))
 
-(defn calcNextIncreasing [prevResults]
-  (loop [k 9 nextResults {9 1}]
-		(if (< k 0)
-			nextResults
-	   (recur (dec k) (updateAll nextResults (range 0 k) (get prevResults k))))))
-
-(defn calcNextDecreasing [prevResults]
-  (loop [k 1 nextResults {0 1}]
-		(if (= k 9)
-     nextResults
-	   (recur (inc k) (updateAll nextResults (range k 10) (get prevResults k))))))
-
-(defn calcCounts [top calcNextFun initial]
-  (loop [n 2 results {1 initial}]
-		(if (> n top)
-			results
-      (recur (inc n) (assoc results n (calcNextFun (get results (dec n))))))))
+(defn calc [x cnt]
+  (if (>= (/ (- (dec x) cnt) (dec x)) 99/100)
+    (dec x)
+    (recur (inc x) (if (isNotBouncy x) (inc cnt) cnt))))
